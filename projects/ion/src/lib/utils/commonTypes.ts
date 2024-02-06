3f6d11fb5f6d5739554fc0c04c3de394f3c7a81a
PedroMiguel7 <pedromiguelmgaldino@gmail.com>
2024-02-06 10:24:59 -0300

    docs: direction type comment in commonTypes.ts

--- projects/ion/src/lib/utils/commonTypes.ts
+++ projects/ion/src/lib/utils/commonTypes.ts
@@ -1,4 +1,4 @@
 /**
- * @typedef { 'horizontal' | 'vertical' } DirectionType - The direction of the divider
+ * @typedef { 'horizontal' | 'vertical' } DirectionType - The direction of the component.
  */
 export type DirectionType = 'horizontal' | 'vertical';
