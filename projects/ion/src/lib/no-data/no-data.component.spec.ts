a63ea3f2dd238a8c66573eb9589139d2885a056e
wermeson-lopes-brisa <wermeson.lopes@grupobrisanet.com.br>
2024-02-08 14:45:11 -0300

    feat: create no data component

---
+++ projects/ion/src/lib/no-data/no-data.component.spec.ts
@@ -0,0 +1,44 @@
+import { ComponentFixture } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular';
+import { IonNoDataComponent } from './no-data.component';
+import { IonNoDataProps } from './types';
+
+const customLabel = 'No data';
+
+const testCustomProps: IonNoDataComponent = {
+  label: customLabel,
+  iconType: 'exclamation-solid',
+};
+
+const sut = async (
+  customProps?: IonNoDataProps
+): Promise<ComponentFixture<IonNoDataComponent>> => {
+  const { fixture } = await render(IonNoDataComponent, {
+    componentProperties: customProps,
+  });
+  return fixture;
+};
+
+describe('IonNoDataComponent - default properties', () => {
+  beforeEach(async () => {
+    await sut();
+  });
+
+  it('Should render the default label when no label is informed', async () => {
+    expect(screen.getByText('Não há dados')).toBeInTheDocument();
+  });
+
+  it('Should render a icon by default', async () => {
+    expect(screen.getByTestId('ion-no-data-icon')).toBeInTheDocument();
+  });
+});
+
+describe('IonNoDataComponent - custom properties', () => {
+  beforeEach(async () => {
+    await sut(testCustomProps);
+  });
+
+  it('Should render the informed label', async () => {
+    expect(screen.getByText(customLabel)).toBeInTheDocument();
+  });
+});
