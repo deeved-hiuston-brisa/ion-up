377ace298ad41261fdba73efdaa4aec2899fc033
wermeson-lopes-brisa <wermeson.lopes@grupobrisanet.com.br>
2024-02-08 14:51:41 -0300

    feat: added story

---
+++ projects/ion/src/stories/no-data.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { IonNoDataComponent, iconsPaths } from '../public-api';
+
+const meta: Meta<IonNoDataComponent> = {
+  title: 'Ion/Data Display/No Data',
+  component: IonNoDataComponent,
+  tags: ['autodocs'],
+  render: (args: IonNoDataComponent) => ({
+    props: {
+      ...args,
+    },
+  }),
+  argTypes: {
+    label: {
+      control: {
+        type: 'text',
+      },
+      description: 'Texto a ser exibido quando não houver dados.',
+    },
+    iconType: {
+      control: {
+        type: 'select',
+      },
+      options: Object.keys(iconsPaths),
+      description:
+        'Nome válido de ícone associado que aparece à esquerda do texto sobre não haver dados. Pode ser qualquer ícone da biblioteca Ion.',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<IonNoDataComponent>;
+export const Default: Story = {
+  args: {} as IonNoDataComponent,
+};
