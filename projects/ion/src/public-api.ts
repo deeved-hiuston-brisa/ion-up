e5411cec15c17a4f2c2779ba9324e5da2ce2f4b7
wermeson-lopes-brisa <wermeson.lopes@grupobrisanet.com.br>
2024-02-09 14:40:57 -0300

    feat: create tooltip component

--- projects/ion/src/public-api.ts
+++ projects/ion/src/public-api.ts
@@ -3,12 +3,13 @@
  */
 
 // export * from './lib/ion.component';
-export * from './lib/badge';
 export * from './lib/alert';
+export * from './lib/badge';
 export * from './lib/button';
-export * from './lib/icon';
-export * from './lib/tab';
 export * from './lib/divider';
-export * from './lib/tag';
+export * from './lib/icon';
 export * from './lib/spinner';
+export * from './lib/tab';
+export * from './lib/tag';
+export * from './lib/tooltip';
 export * from './lib/utils/commonTypes';
