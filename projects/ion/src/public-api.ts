931c02f266e54ae1a2389c379e82a8b435713181
wermeson-lopes-brisa <wermeson.lopes@grupobrisanet.com.br>
2024-02-20 09:39:58 -0300

    changed for only re-run lint test

--- projects/ion/src/public-api.ts
+++ projects/ion/src/public-api.ts
@@ -4,18 +4,18 @@
 
 // export * from './lib/ion.component';
 export * from './lib/alert';
+export * from './lib/avatar';
 export * from './lib/badge';
 export * from './lib/breadcrumb';
 export * from './lib/button';
 export * from './lib/divider';
 export * from './lib/icon';
+export * from './lib/link';
 export * from './lib/no-data';
 export * from './lib/notification';
-export * from './lib/spinner';
-export * from './lib/tab';
-export * from './lib/link';
-export * from './lib/tag';
-export * from './lib/switch';
-export * from './lib/avatar';
-export * from './lib/utils/commonTypes';
 export * from './lib/sidebar';
+export * from './lib/spinner';
+export * from './lib/switch';
+export * from './lib/tab';
+export * from './lib/tag';
+export * from './lib/utils/commonTypes';
