8072c40a11c543d60981cb2fc50f541b4637b2a6
danilo-moreira-brisa <danilo.moreira@grupobrisanet.com.br>
2024-06-18 10:17:04 -0300

    refactor: use signals on ion-alert

--- projects/ion/src/lib/alert/alert.component.ts
+++ projects/ion/src/lib/alert/alert.component.ts
@@ -1,9 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  computed,
+  input,
+  viewChild,
+} from '@angular/core';
 import { IconType, IonIconComponent } from '../icon';
-import { IonAlertProps } from './types';
+import { IonAlertProps, IonAlertStatus } from './types';
 
-export const alertIconTypes = {
+export const alertIconTypes: Record<IonAlertStatus, IconType> = {
   success: 'check-solid',
   warning: 'exclamation-solid',
   info: 'info-solid',
@@ -16,31 +22,22 @@ export const alertIconTypes = {
   templateUrl: './alert.component.html',
   styleUrl: './alert.component.scss',
   imports: [CommonModule, IonIconComponent],
-  host: {
-    '[attr.data-type]': 'type',
-  },
+  host: { '[attr.data-type]': 'type()' },
 })
-export class IonAlertComponent implements OnInit {
-  @Input() message?: IonAlertProps['message'];
-  @Input() customBody?: IonAlertProps['customBody'];
-  @Input() type?: IonAlertProps['type'] = 'success';
-  @Input() description?: IonAlertProps['description'];
-  @Input() closable?: IonAlertProps['closable'] = false;
-  @Input() hideBackground?: IonAlertProps['hideBackground'] = false;
-  @Input() noRadius?: IonAlertProps['noRadius'] = false;
-  @ViewChild('ionAlert', { static: false }) private ionAlert!: ElementRef;
+export class IonAlertComponent {
+  message = input<IonAlertProps['message']>();
+  customBody = input<IonAlertProps['customBody']>();
+  description = input<IonAlertProps['description']>();
+  closable = input<IonAlertProps['closable']>(false);
+  hideBackground = input<IonAlertProps['hideBackground']>(false);
+  noRadius = input<IonAlertProps['noRadius']>(false);
+  type = input<IonAlertProps['type']>('success');
 
-  public iconType!: IconType;
+  ionAlert = viewChild<ElementRef>('ionAlert');
 
-  setIconType(): void {
-    this.iconType = alertIconTypes[this.type!];
-  }
+  iconType = computed<IconType>(() => alertIconTypes[this.type()]);
 
   closeEvent(): void {
-    this.ionAlert.nativeElement.remove();
-  }
-
-  ngOnInit(): void {
-    this.setIconType();
+    this.ionAlert()?.nativeElement.remove();
   }
 }
