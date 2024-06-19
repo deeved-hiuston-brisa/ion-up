9237f3f60526c2667f1c8febb0992fcfcb7ed38d
Danilo Alves <138066684+danilo-moreira-brisa@users.noreply.github.com>
2024-06-19 08:33:15 -0300

    refactor: use signals on ion-alert (#100)
    * refactor: use signals on ion-alert
    
    * feat: edit changeDetection to onPush

--- projects/ion/src/lib/alert/types.ts
+++ projects/ion/src/lib/alert/types.ts
@@ -8,7 +8,7 @@ export interface IonAlertProps {
    * @param message - Text that will be displayed on the Alert.
    * @type {string}
    */
-  message?: string;
+  message: string;
   /**
    * Alert custom body.
    * @param customBody - HTML content that will be rendered instead of the simple message.
@@ -19,39 +19,39 @@ export interface IonAlertProps {
         <h1>Custom alert</h1>
       </ng-template>
    */
-  customBody?: TemplateRef<void>;
+  customBody: TemplateRef<void>;
   /**
    * Alert description.
    * @param description - Text that will be displayed in below the main alert text.
    * @type {string}
    */
-  description?: string;
+  description: string;
   /**
    * Alert type.
    * @param type - Defines the Alert style.
    * @type {'success' | 'info' | 'warning' | 'negative'}
    * @default `success`
    */
-  type?: IonAlertStatus;
+  type: IonAlertStatus;
   /**
    * Alert closable.
    * @param type - Defines if the Alert can be closed.
    * @type {boolean}
    * @default `false`
    */
-  closable?: boolean;
+  closable: boolean;
   /**
    * Alert hideBackground.
    * @param hideBackground - Defines if the Alert will have a transparent background.
    * @type {boolean}
    * @default `false`
    */
-  hideBackground?: boolean;
+  hideBackground: boolean;
   /**
    * Alert noRadius.
    * @param noRadius - Defines if the Alert won't have a border radius.
    * @type {boolean}
    * @default `false`
    */
-  noRadius?: boolean;
+  noRadius: boolean;
 }
