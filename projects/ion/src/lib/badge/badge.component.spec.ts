b9c489cc33547fad5aea54e3328e47f1cb9e7e5a
danilo-moreira-brisa <danilo.moreira@grupobrisanet.com.br>
2024-06-20 13:09:30 -0300

    test: improving cases using sut

--- projects/ion/src/lib/badge/badge.component.spec.ts
+++ projects/ion/src/lib/badge/badge.component.spec.ts
@@ -9,37 +9,29 @@ const types: Array<IonBadgeProps['type']> = [
   'negative',
 ];
 
-const sut = async (
-  customProps: Partial<IonBadgeProps>
-): Promise<HTMLElement> => {
+const sut = async (customProps: Partial<IonBadgeProps>): Promise<void> => {
   await render(IonBadgeComponent, {
     componentInputs: { ...customProps },
-    autoDetectChanges: true,
   });
-
-  return screen.getByTestId(`ion-badge-${customProps.label}`);
 };
 
 describe('IonBadgeComponent', () => {
   it.each(['text badge', 10, 30])(
     'should render a badge with label %s',
     async label => {
-      await render(IonBadgeComponent, {
-        componentInputs: { label },
-      });
+      await sut({ label });
       expect(screen.getByText(label)).toBeInTheDocument();
     }
   );
 
   it('should render 99+ when label is bigger than 99', async () => {
-    await render(IonBadgeComponent, {
-      componentInputs: { label: 1000 },
-    });
+    await sut({ label: 1000 });
     expect(screen.getByText('99+')).toBeInTheDocument();
   });
 
   it.each(types)('should correctly render badge %s types.', async type => {
-    expect(await sut({ label: `type`, type })).toHaveAttribute(
+    await sut({ label: type, type });
+    expect(screen.getByTestId(`ion-badge-${type}`)).toHaveAttribute(
       'data-type',
       type
     );
