d77a7f4f05bcc4d7e98ce022d65c00d9c1ab8c5b
Iury Nogueira <nogueiraiury20@gmail.com>
2025-01-29 11:45:40 -0300

    fix: remove mock exporter

--- projects/ion/src/lib/notification/public-api.ts
+++ projects/ion/src/lib/notification/public-api.ts
@@ -1,4 +1,3 @@
 export * from './component/notification.component';
-export * from './mock/open-notification-mock.component';
 export * from './service/notification.service';
 export * from './types';
